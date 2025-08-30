2c7754d389cceb189769ea765d845988bdb336ad
AdamLoughnane <60343027+AdamLoughnane@users.noreply.github.com>
2025-08-30 20:49:03 +0100

    Initial Commit

---
+++ Chekoff/ChekoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChekoffApp.swift
+//  Chekoff
+//
+//  Created by Adam Loughnane on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChekoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChekoffTests/ChekoffTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChekoffTests.swift
+//  ChekoffTests
+//
+//  Created by Adam Loughnane on 30/08/2025.
+//
+
+import Testing
+@testable import Chekoff
+
+struct ChekoffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
